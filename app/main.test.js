4c28ff5e6e05bda0a503d23b3d173f59735c465c
k2works <kakimomokuri@gmail.com>
2025-06-24 08:55:39 +0000

    refactor: テストコードのリファクタリング - beforeEachでFizzBuzzクラスの参照を共有

--- app/main.test.js
+++ app/main.test.js
@@ -7,11 +7,17 @@ test('greeting returns hello world', () => {
 
 // FizzBuzzテスト
 describe('FizzBuzz', () => {
+  let fizzbuzz;
+
+  beforeEach(() => {
+    fizzbuzz = FizzBuzz;
+  });
+
   test('1を渡したら文字列1を返す', () => {
-    expect(FizzBuzz.generate(1)).toBe('1');
+    expect(fizzbuzz.generate(1)).toBe('1');
   });
 
   test('2を渡したら文字列2を返す', () => {
-    expect(FizzBuzz.generate(2)).toBe('2');
+    expect(fizzbuzz.generate(2)).toBe('2');
   });
 });
