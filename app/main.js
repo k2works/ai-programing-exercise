0a9311e468d53e418d934b2263264406bc778b30
k2works <kakimomokuri@gmail.com>
2025-06-24 09:04:26 +0000

    feat: 3と5の両方の倍数でFizzBuzzを返す機能を実装 - 基本的なFizzBuzzロジック完成

--- app/main.js
+++ app/main.js
@@ -6,6 +6,9 @@ function greeting() {
 // FizzBuzzクラス
 class FizzBuzz {
   static generate(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      return 'FizzBuzz';
+    }
     if (number % 3 === 0) {
       return 'Fizz';
     }
