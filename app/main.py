3aabc99edfd6f85334d90a4268040bc1875ded27
k2works <kakimomokuri@gmail.com>
2025-06-25 02:20:45 +0000

    test: 3と5の倍数の実装

--- app/main.py
+++ app/main.py
@@ -9,10 +9,25 @@ class TestFizzBuzz:
 
     def test_2を渡したら文字列2を返す(self):
         assert self.fizzbuzz.generate(2) == '2'
+    
+    def test_3を渡したら文字列Fizzを返す(self):
+        assert self.fizzbuzz.generate(3) == 'Fizz'
+    
+    def test_5を渡したら文字列Buzzを返す(self):
+        assert self.fizzbuzz.generate(5) == 'Buzz'
+    
+    def test_15を渡したら文字列FizzBuzzを返す(self):
+        assert self.fizzbuzz.generate(15) == 'FizzBuzz'
 
 class FizzBuzz:
     @staticmethod
     def generate(number):
+        if number % 15 == 0:
+            return 'FizzBuzz'
+        if number % 3 == 0:
+            return 'Fizz'
+        if number % 5 == 0:
+            return 'Buzz'
         return str(number)
 
 if __name__ == '__main__':
