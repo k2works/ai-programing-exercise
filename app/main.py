297184a9166bf860e9f8723866cbb4c783a7f177
k2works <kakimomokuri@gmail.com>
2025-06-25 02:23:17 +0000

    feat: 1から100までの数をプリントする機能完成

--- app/main.py
+++ app/main.py
@@ -18,6 +18,15 @@ class TestFizzBuzz:
     
     def test_15を渡したら文字列FizzBuzzを返す(self):
         assert self.fizzbuzz.generate(15) == 'FizzBuzz'
+    
+    def test_1から100までの数を文字列で返す(self):
+        result = self.fizzbuzz.generate_list()
+        assert len(result) == 100
+        assert result[0] == '1'
+        assert result[1] == '2'
+        assert result[2] == 'Fizz'
+        assert result[4] == 'Buzz'
+        assert result[14] == 'FizzBuzz'
 
 class FizzBuzz:
     @staticmethod
@@ -29,6 +38,20 @@ class FizzBuzz:
         if number % 5 == 0:
             return 'Buzz'
         return str(number)
+    
+    @staticmethod
+    def generate_list():
+        return [FizzBuzz.generate(i) for i in range(1, 101)]
+    
+    @staticmethod
+    def print_fizzbuzz():
+        result = FizzBuzz.generate_list()
+        for item in result:
+            print(item)
 
 if __name__ == '__main__':
-    pytest.main([__file__])
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        pytest.main([__file__])
+    else:
+        FizzBuzz.print_fizzbuzz()
