2e33eac333419958e26a8be149a623696288e0e6
k2works <kakimomokuri@gmail.com>
2025-06-25 02:15:21 +0000

    test: 数を文字列にして返す

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 import pytest
 
-def test_greeting():
-    assert greeting() == 'hello world'
+def test_1を渡したら文字列1を返す():
+    assert FizzBuzz.generate(1) == '1'
 
-def greeting():
-    return 'hello world'
+def test_2を渡したら文字列2を返す():
+    assert FizzBuzz.generate(2) == '2'
+
+class FizzBuzz:
+    @staticmethod
+    def generate(number):
+        return str(number)
 
 if __name__ == '__main__':
     pytest.main([__file__])
