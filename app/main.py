73946d4a113079f52ad4b155e8a8c322c4885ed0
k2works <kakimomokuri@gmail.com>
2025-06-25 02:16:13 +0000

    refactor: メソッドの抽出

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import pytest
 
-def test_1を渡したら文字列1を返す():
-    assert FizzBuzz.generate(1) == '1'
+class TestFizzBuzz:
+    def setup_method(self):
+        self.fizzbuzz = FizzBuzz
+    
+    def test_1を渡したら文字列1を返す(self):
+        assert self.fizzbuzz.generate(1) == '1'
 
-def test_2を渡したら文字列2を返す():
-    assert FizzBuzz.generate(2) == '2'
+    def test_2を渡したら文字列2を返す(self):
+        assert self.fizzbuzz.generate(2) == '2'
 
 class FizzBuzz:
     @staticmethod
