5fb596980536281b9dc4353f4bd09d69c59b2e16
k2works <kakimomokuri@gmail.com>
2025-06-26 09:14:15 +0000

    test: 3の倍数のときはFizzを返す

--- app/main.go
+++ app/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 // FizzBuzzクラスに相当する関数
 func Generate(number int) string {
+	if number%3 == 0 {
+		return "Fizz"
+	}
 	return fmt.Sprintf("%d", number)
 }
 
--- app/main_test.go
+++ app/main_test.go
@@ -20,3 +20,8 @@ func Test1を渡したら文字列1を返す(t *testing.T) {
 func Test2を渡したら文字列2を返す(t *testing.T) {
 	assertGenerate(t, 2, "2")
 }
+
+// テスト関数：3を渡したら文字列"Fizz"を返す
+func Test3を渡したらFizzを返す(t *testing.T) {
+	assertGenerate(t, 3, "Fizz")
+}
