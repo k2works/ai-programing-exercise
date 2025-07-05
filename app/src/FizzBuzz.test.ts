0a0ba28db412604f55ad26e56809d51148edb67e
k2works <kakimomokuri@gmail.com>
2025-07-05 05:33:37 +0000

    仮実装 - 1を渡したら文字列'1'を返すテストを追加し、仮実装でテストを通す

---
+++ app/src/FizzBuzz.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { FizzBuzz } from './FizzBuzz'
+
+describe('FizzBuzz', () => {
+  let fizzBuzz: FizzBuzz
+
+  beforeEach(() => {
+    fizzBuzz = new FizzBuzz()
+  })
+
+  describe('数を文字列にして返す', () => {
+    it('1を渡したら文字列"1"を返す', () => {
+      expect(fizzBuzz.generate(1)).toBe('1')
+    })
+  })
+})
---
+++ app/src/FizzBuzz.ts
@@ -0,0 +1,5 @@
+export class FizzBuzz {
+  generate(number: number): string {
+    return '1'
+  }
+}
