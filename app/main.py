2c500e7b558061c661c01e2f1398a80da4a94fd6
k2works <kakimomokuri@gmail.com>
2025-07-11 01:50:19 +0000

    test: 数を文字列にして返す

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 class FizzBuzz:
     @staticmethod
     def generate(number):
-        return '1'
+        return str(number)
 
 def test_1を渡したら文字列1を返す():
     assert FizzBuzz.generate(1) == '1'
+
+def test_2を渡したら文字列2を返す():
+    assert FizzBuzz.generate(2) == '2'
