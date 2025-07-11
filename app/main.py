e527776c8811fd57b563197f64de62d1fc65deb8
k2works <kakimomokuri@gmail.com>
2025-07-11 01:53:10 +0000

    test: 3と5の倍数の場合にFizzBuzzと返す

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 class FizzBuzz:
     @staticmethod
     def generate(number):
+        if number % 15 == 0:
+            return 'FizzBuzz'
         if number % 3 == 0:
             return 'Fizz'
         if number % 5 == 0:
@@ -22,3 +24,6 @@ class TestFizzBuzz:
     
     def test_5を渡したら文字列Buzzを返す(self):
         assert self.fizzbuzz.generate(5) == 'Buzz'
+    
+    def test_15を渡したら文字列FizzBuzzを返す(self):
+        assert self.fizzbuzz.generate(15) == 'FizzBuzz'
