da91071bf0719f829bc8f58230725eda3b31bc1e
k2works <kakimomokuri@gmail.com>
2025-07-15 01:45:45 +0000

    refactor: テスト整理

--- app/main.test.js
+++ app/main.test.js
@@ -7,23 +7,31 @@ describe('FizzBuzz', () => {
     fizzbuzz = FizzBuzz;
   });
 
-  test('1を渡したら文字列1を返す', () => {
-    expect(fizzbuzz.generate(1)).toBe('1');
+  describe('三の倍数の場合', () => {
+    test('3を渡したら文字列Fizzを返す', () => {
+      expect(fizzbuzz.generate(3)).toBe('Fizz');
+    });
   });
 
-  test('2を渡したら文字列2を返す', () => {
-    expect(fizzbuzz.generate(2)).toBe('2');
+  describe('五の倍数の場合', () => {
+    test('5を渡したら文字列Buzzを返す', () => {
+      expect(fizzbuzz.generate(5)).toBe('Buzz');
+    });
   });
 
-  test('3を渡したら文字列Fizzを返す', () => {
-    expect(fizzbuzz.generate(3)).toBe('Fizz');
+  describe('三と五の倍数の場合', () => {
+    test('15を渡したら文字列FizzBuzzを返す', () => {
+      expect(fizzbuzz.generate(15)).toBe('FizzBuzz');
+    });
   });
 
-  test('5を渡したら文字列Buzzを返す', () => {
-    expect(fizzbuzz.generate(5)).toBe('Buzz');
-  });
+  describe('その他の場合', () => {
+    test('1を渡したら文字列1を返す', () => {
+      expect(fizzbuzz.generate(1)).toBe('1');
+    });
 
-  test('15を渡したら文字列FizzBuzzを返す', () => {
-    expect(fizzbuzz.generate(15)).toBe('FizzBuzz');
+    test('2を渡したら文字列2を返す', () => {
+      expect(fizzbuzz.generate(2)).toBe('2');
+    });
   });
 });
