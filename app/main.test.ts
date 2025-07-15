ce693e0e358bf776c2956f0d53ef7f9dfe899b41
k2works <kakimomokuri@gmail.com>
2025-07-15 04:04:36 +0000

    refactor: メソッドの抽出

--- app/main.test.ts
+++ app/main.test.ts
@@ -1,11 +1,17 @@
 import { FizzBuzz } from './fizzbuzz';
 
 describe('FizzBuzzTest', () => {
+  let fizzbuzz: typeof FizzBuzz;
+
+  beforeEach(() => {
+    fizzbuzz = FizzBuzz;
+  });
+
   test('1を渡したら文字列1を返す', () => {
-    expect(FizzBuzz.generate(1)).toBe('1');
+    expect(fizzbuzz.generate(1)).toBe('1');
   });
 
   test('2を渡したら文字列2を返す', () => {
-    expect(FizzBuzz.generate(2)).toBe('2');
+    expect(fizzbuzz.generate(2)).toBe('2');
   });
 });
