bc47d991b25af4d4c57df791dc3b28fd72a0969d
k2works <kakimomokuri@gmail.com>
2025-07-15 08:05:15 +0000

    test: 15を渡したら文字列FizzBuzzを返す

--- app/fizzbuzz.py
+++ app/fizzbuzz.py
@@ -2,7 +2,9 @@ class FizzBuzz:
     @staticmethod
     def generate(number):
         result = str(number)
-        if number % 3 == 0:
+        if number % 3 == 0 and number % 5 == 0:
+            result = 'FizzBuzz'
+        elif number % 3 == 0:
             result = 'Fizz'
         elif number % 5 == 0:
             result = 'Buzz'
--- app/test_fizzbuzz.py
+++ app/test_fizzbuzz.py
@@ -18,6 +18,9 @@ class FizzBuzzTest(unittest.TestCase):
     def test_5を渡したら文字列Buzzを返す(self):
         self.assertEqual('Buzz', self.fizzbuzz.generate(5))
 
+    def test_15を渡したら文字列FizzBuzzを返す(self):
+        self.assertEqual('FizzBuzz', self.fizzbuzz.generate(15))
+
 
 if __name__ == '__main__':
     unittest.main()
