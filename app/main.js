d5714a194b3131b36de25af72b60cf01984477f4
k2works <kakimomokuri@gmail.com>
2025-07-15 01:44:57 +0000

    test: 15を渡したら文字列FizzBuzzを返す

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 class FizzBuzz {
   static generate(number) {
     let result = number.toString();
-    if (number % 3 === 0) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      result = 'FizzBuzz';
+    } else if (number % 3 === 0) {
       result = 'Fizz';
     } else if (number % 5 === 0) {
       result = 'Buzz';
--- app/main.test.js
+++ app/main.test.js
@@ -22,4 +22,8 @@ describe('FizzBuzz', () => {
   test('5を渡したら文字列Buzzを返す', () => {
     expect(fizzbuzz.generate(5)).toBe('Buzz');
   });
+
+  test('15を渡したら文字列FizzBuzzを返す', () => {
+    expect(fizzbuzz.generate(15)).toBe('FizzBuzz');
+  });
 });
