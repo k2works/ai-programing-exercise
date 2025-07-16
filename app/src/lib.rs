713e169ac79892e150eb4a47ee0e1fa61c97b511
k2works <kakimomokuri@gmail.com>
2025-07-16 06:21:32 +0000

    chore: タスクの自動化セットアップ

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -24,6 +24,11 @@ mod tests {
         fn test_1を渡したら文字列1を返す() {
             assert_eq!("1", FizzBuzz::generate(1));
         }
+
+        #[test]
+        fn test_2を渡したら文字列2を返す() {
+            assert_eq!("2", FizzBuzz::generate(2));
+        }
     }
 }
 
