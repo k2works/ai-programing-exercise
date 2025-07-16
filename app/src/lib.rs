420967f0e421c864739bd6a1d862f6981c11645e
k2works <kakimomokuri@gmail.com>
2025-07-16 06:16:00 +0000

    feat: 5を渡したら文字列Buzzを返す

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -9,13 +9,22 @@ mod tests {
         fn test_3を渡したら文字列fizzを返す() {
             assert_eq!("Fizz", FizzBuzz::generate(3));
         }
+
+        #[test]
+        fn test_5を渡したら文字列buzzを返す() {
+            assert_eq!("Buzz", FizzBuzz::generate(5));
+        }
     }
 }
 
 pub struct FizzBuzz;
 
 impl FizzBuzz {
-    pub fn generate(_number: i32) -> &'static str {
-        "Fizz"
+    pub fn generate(number: i32) -> &'static str {
+        if number % 5 == 0 {
+            "Buzz"
+        } else {
+            "Fizz"
+        }
     }
 }
