861d1b6879c7184c591b2d53fa459ad4bd8113b2
k2works <kakimomokuri@gmail.com>
2025-07-16 05:41:39 +0000

    test: セットアップ

---
+++ app/fizzbuzz/src/main.rs
@@ -0,0 +1,5 @@
+use fizzbuzz::greeting;
+
+fn main() {
+    println!("{}", greeting());
+}
---
+++ app/fizzbuzz/tests/hello_test.rs
@@ -0,0 +1,6 @@
+use fizzbuzz::greeting;
+
+#[test]
+fn test_greeting() {
+    assert_eq!("hello world", greeting());
+}
