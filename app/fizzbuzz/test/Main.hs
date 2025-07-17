ea0426571b6eaa8c089c357043139459ae63627d
k2works <kakimomokuri@gmail.com>
2025-07-17 04:54:25 +0000

    feat: 自動化ツールのセットアップ
    - HLintによる静的解析の導入
    - stylish-haskellによるコードフォーマッタの導入
    - hpcによるコードカバレッジ測定の導入
    - Makefileによるタスクランナーの設定
    
    三種の神器の第三の要素「自動化」の実装

--- app/fizzbuzz/test/Main.hs
+++ app/fizzbuzz/test/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
-import Test.Hspec
-import FizzBuzz
+import           FizzBuzz
+import           Test.Hspec
 
 main :: IO ()
 main = hspec $ do
@@ -10,22 +10,22 @@ main = hspec $ do
       it "15を渡したら文字列\"FizzBuzz\"を返す" $ do
         let fizzbuzz = generate
         fizzbuzz 15 `shouldBe` "FizzBuzz"
-        
+
     describe "三の倍数の場合" $ do
       it "3を渡したら文字列\"Fizz\"を返す" $ do
         let fizzbuzz = generate
         fizzbuzz 3 `shouldBe` "Fizz"
-        
+
     describe "五の倍数の場合" $ do
       it "5を渡したら文字列\"Buzz\"を返す" $ do
         let fizzbuzz = generate
         fizzbuzz 5 `shouldBe` "Buzz"
-        
+
     describe "その他の場合" $ do
       it "1を渡したら文字列\"1\"を返す" $ do
         let fizzbuzz = generate
         fizzbuzz 1 `shouldBe` "1"
-        
+
       it "2を渡したら文字列\"2\"を返す" $ do
         let fizzbuzz = generate
         fizzbuzz 2 `shouldBe` "2"
