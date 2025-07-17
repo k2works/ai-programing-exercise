f59c88faf3bff218c12dbddd19a457b81be2507c
k2works <kakimomokuri@gmail.com>
2025-07-17 02:11:08 +0000

    test: タイプ2の場合

--- app/fizz-buzz/src/fizz_buzz/core.clj
+++ app/fizz-buzz/src/fizz_buzz/core.clj
@@ -4,13 +4,16 @@
 (defn generate 
   ([number] (generate number 1))
   ([number type]
-   (let [fizz? (zero? (mod number 3))
-         buzz? (zero? (mod number 5))]
-     (cond
-       (and fizz? buzz?) "FizzBuzz"
-       fizz? "Fizz"
-       buzz? "Buzz"
-       :else (str number)))))
+   (cond
+     (= type 2) (str number)
+     :else
+     (let [fizz? (zero? (mod number 3))
+           buzz? (zero? (mod number 5))]
+       (cond
+         (and fizz? buzz?) "FizzBuzz"
+         fizz? "Fizz"
+         buzz? "Buzz"
+         :else (str number))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- app/fizz-buzz/test/fizz_buzz/core_test.clj
+++ app/fizz-buzz/test/fizz_buzz/core_test.clj
@@ -37,4 +37,13 @@
       (testing "5を渡したら文字列Buzzを返す"
         (is (= "Buzz" (generate 5 1))))
       (testing "15を渡したら文字列FizzBuzzを返す"
-        (is (= "FizzBuzz" (generate 15 1)))))))
+        (is (= "FizzBuzz" (generate 15 1)))))
+  (testing "タイプ2の場合"
+    (testing "1を渡したら文字列1を返す"
+      (is (= "1" (generate 1 2))))
+    (testing "3を渡したら文字列3を返す"
+      (is (= "3" (generate 3 2))))
+    (testing "5を渡したら文字列5を返す"
+      (is (= "5" (generate 5 2))))
+    (testing "15を渡したら文字列15を返す"
+      (is (= "15" (generate 15 2)))))))
