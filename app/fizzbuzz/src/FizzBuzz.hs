3018d1c7b375a66195d18bd41478a6c4fcad268d
k2works <kakimomokuri@gmail.com>
2025-07-17 04:33:17 +0000

    test: 5を渡したら文字列Buzzを返す

--- app/fizzbuzz/src/FizzBuzz.hs
+++ app/fizzbuzz/src/FizzBuzz.hs
@@ -1,7 +1,7 @@
 module FizzBuzz (generate) where
 
 generate :: Int -> String
-generate number =
-  case (number `mod` 3 == 0) of
-    True -> "Fizz"
-    False -> show number
+generate number
+  | number `mod` 3 == 0 = "Fizz"
+  | number `mod` 5 == 0 = "Buzz"
+  | otherwise = show number
--- app/fizzbuzz/test/Main.hs
+++ app/fizzbuzz/test/Main.hs
@@ -1,20 +1,26 @@
 module Main (main) where
 
 import Test.Hspec
-import FizzBuzz (generate)
+import FizzBuzz
 
 main :: IO ()
 main = hspec $ do
   describe "FizzBuzz" $ do
-    let fizzbuzz = generate
-    
     describe "三の倍数の場合" $ do
       it "3を渡したら文字列\"Fizz\"を返す" $ do
+        let fizzbuzz = generate
         fizzbuzz 3 `shouldBe` "Fizz"
-    
+        
+    describe "五の倍数の場合" $ do
+      it "5を渡したら文字列\"Buzz\"を返す" $ do
+        let fizzbuzz = generate
+        fizzbuzz 5 `shouldBe` "Buzz"
+        
     describe "その他の場合" $ do
       it "1を渡したら文字列\"1\"を返す" $ do
+        let fizzbuzz = generate
         fizzbuzz 1 `shouldBe` "1"
-      
+        
       it "2を渡したら文字列\"2\"を返す" $ do
+        let fizzbuzz = generate
         fizzbuzz 2 `shouldBe` "2"
