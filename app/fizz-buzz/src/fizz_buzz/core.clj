8f765b59ac0f8f8dc2e282c0743030a7158f85fb
k2works <kakimomokuri@gmail.com>
2025-07-17 02:40:10 +0000

    refactor: オブジェクトによるプリミティブの置き換え

--- app/fizz-buzz/src/fizz_buzz/core.clj
+++ app/fizz-buzz/src/fizz_buzz/core.clj
@@ -1,45 +1,56 @@
 (ns fizz-buzz.core
   (:gen-class))
 
+; 値オブジェクト
+(defrecord FizzBuzzValue [value])
+
+(defn create-fizz-buzz-value [value]
+  (->FizzBuzzValue value))
+
 ; 数値判定ユーティリティ関数
-(defn fizz? [number] (zero? (mod number 3)))
-(defn buzz? [number] (zero? (mod number 5)))
-(defn fizz-buzz? [number] (and (fizz? number) (buzz? number)))
+(defn fizz? [fizz-buzz-value]
+  (zero? (mod (:value fizz-buzz-value) 3)))
+
+(defn buzz? [fizz-buzz-value]
+  (zero? (mod (:value fizz-buzz-value) 5)))
+
+(defn fizz-buzz? [fizz-buzz-value]
+  (and (fizz? fizz-buzz-value) (buzz? fizz-buzz-value)))
 
 ; ポリモーフィズムの準備: Typeに対応するプロトコル
 (defprotocol FizzBuzzType
-  (generate-string [this number]))
+  (generate-string [this fizz-buzz-value]))
 
 ; FizzBuzzの通常パターン実装
 (defrecord FizzBuzzType01 []
   FizzBuzzType
-  (generate-string [this number]
-    (cond
-      (fizz-buzz? number) "FizzBuzz"
-      (fizz? number) "Fizz"
-      (buzz? number) "Buzz"
-      :else (str number))))
+  (generate-string [this fizz-buzz-value]
+                 (cond
+      (fizz-buzz? fizz-buzz-value) "FizzBuzz"
+                                (fizz? fizz-buzz-value) "Fizz"
+                                (buzz? fizz-buzz-value) "Buzz"
+                                :else (str (:value fizz-buzz-value)))))
 
 ; 数字のみ実装
 (defrecord FizzBuzzType02 []
   FizzBuzzType
-  (generate-string [this number]
-    (str number)))
+  (generate-string [this fizz-buzz-value]
+                  (str (:value fizz-buzz-value))))
 
 ; FizzBuzzのみ実装
 (defrecord FizzBuzzType03 []
   FizzBuzzType
-  (generate-string [this number]
-    (if (fizz-buzz? number)
-      "FizzBuzz"
-      (str number))))
+  (generate-string [this fizz-buzz-value]
+                 (if (fizz-buzz? fizz-buzz-value)
+                   "FizzBuzz"
+      (str (:value fizz-buzz-value)))))
 
 ; TypeFactoryメソッド
 (defn create-type [type-id]
   (case type-id
     1 (->FizzBuzzType01)
-    2 (->FizzBuzzType02)
-    3 (->FizzBuzzType03)
+2 (->FizzBuzzType02)
+3 (->FizzBuzzType03)
     (throw (Exception. "不正なタイプです"))))
 
 (defrecord FizzBuzz [number type-obj])
@@ -51,12 +62,14 @@
 (defn generate 
   ([number] (generate number 1))
   ([number type]
-   (let [type-obj (create-type type)]
-     (generate-string type-obj number))))
+   (let [fizz-buzz-value (create-fizz-buzz-value number)
+        type-obj (create-type type)]
+    (generate-string type-obj fizz-buzz-value))))
 
 (defn fizz-buzz-generate [fb]
-  (let [{:keys [number type-obj]} fb]
-    (generate-string type-obj number)))
+  (let [{:keys [number type-obj]} fb
+       fizz-buzz-value (create-fizz-buzz-value number)]
+   (generate-string type-obj fizz-buzz-value)))
 
 (defn -main
   "I don't do a whole lot ... yet."
