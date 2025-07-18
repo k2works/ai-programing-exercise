21996f97326f0599622cf8920e42d7c797642a91
k2works <kakimomokuri@gmail.com>
2025-07-18 00:31:49 +0000

    refactor: メソッドの抽出

--- app/fizzbuzz-scala/src/test/scala/FizzBuzzTest.scala
+++ app/fizzbuzz-scala/src/test/scala/FizzBuzzTest.scala
@@ -2,11 +2,13 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class FizzBuzzTest extends AnyFlatSpec with Matchers {
+  val fizzbuzz = FizzBuzz
+
   "generate" should "1を渡したら文字列1を返す" in {
-    FizzBuzz.generate(1) shouldEqual "1"
+    fizzbuzz.generate(1) shouldEqual "1"
   }
 
   it should "2を渡したら文字列2を返す" in {
-    FizzBuzz.generate(2) shouldEqual "2"
+    fizzbuzz.generate(2) shouldEqual "2"
   }
 }
