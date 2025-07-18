e04ccfc3bc5c631b4c85cb8bceba865913454ee1
k2works <kakimomokuri@gmail.com>
2025-07-18 07:04:26 +0000

    refactor: メソッドの抽出

--- app/kotlin-fizzbuzz/src/test/kotlin/FizzBuzzTest.kt
+++ app/kotlin-fizzbuzz/src/test/kotlin/FizzBuzzTest.kt
@@ -1,14 +1,22 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 
 class FizzBuzzTest {
+    private lateinit var fizzbuzz: FizzBuzz.Companion
+
+    @BeforeEach
+    fun setup() {
+        fizzbuzz = FizzBuzz
+    }
+
     @Test
     fun `test_1を渡したら文字列1を返す`() {
-        assertEquals("1", FizzBuzz.generate(1))
+        assertEquals("1", fizzbuzz.generate(1))
     }
 
     @Test
     fun `test_2を渡したら文字列2を返す`() {
-        assertEquals("2", FizzBuzz.generate(2))
+        assertEquals("2", fizzbuzz.generate(2))
     }
 }
