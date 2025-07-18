6a553eb39a37804b7061fb6112a32ee0baa5b082
k2works <kakimomokuri@gmail.com>
2025-07-18 03:03:19 +0000

    refactor: ポリモーフィズムの実装 - case文をStrategyパターンで置き換え

--- app/fizzbuzz-scala/src/main/scala/FizzBuzz.scala
+++ app/fizzbuzz-scala/src/main/scala/FizzBuzz.scala
@@ -1,28 +1,41 @@
-class FizzBuzz(private val fizzBuzzType: Int = 1) {
+trait FizzBuzzType {
+  def generate(number: Int): String
+}
+
+object FizzBuzzType01 extends FizzBuzzType {
+  def generate(number: Int): String = {
+    if (number % 15 == 0) {
+      "FizzBuzz"
+    } else if (number % 3 == 0) {
+      "Fizz"
+    } else if (number % 5 == 0) {
+      "Buzz"
+    } else {
+      number.toString
+    }
+  }
+}
+
+object FizzBuzzType02 extends FizzBuzzType {
+  def generate(number: Int): String = {
+    number.toString
+  }
+}
+
+object FizzBuzzType03 extends FizzBuzzType {
+  def generate(number: Int): String = {
+    if (number % 15 == 0) {
+      "FizzBuzz"
+    } else {
+      number.toString
+    }
+  }
+}
+
+class FizzBuzz(private val fizzBuzzType: FizzBuzzType) {
   
   def generate(number: Int): String = 
-    fizzBuzzType match {
-      case 1 =>
-        if (number % 15 == 0) {
-          "FizzBuzz"
-        } else if (number % 3 == 0) {
-          "Fizz"
-        } else if (number % 5 == 0) {
-          "Buzz"
-        } else {
-          number.toString
-        }
-      case 2 =>
-        number.toString
-      case 3 =>
-        if (number % 15 == 0) {
-          "FizzBuzz"
-        } else {
-          number.toString
-        }
-      case _ =>
-        throw new RuntimeException("引数は1から3までです")
-    }
+    fizzBuzzType.generate(number)
 
   def createList(start: Int, end: Int): Array[String] =
     (start to end).map(generate(_)).toArray
@@ -30,12 +43,18 @@ class FizzBuzz(private val fizzBuzzType: Int = 1) {
 
 object FizzBuzz {
   def generate(number: Int, fizzBuzzType: Int = 1): String = {
-    val fizzbuzz = new FizzBuzz(fizzBuzzType)
+    val typeStrategy = fizzBuzzType match {
+      case 1 => FizzBuzzType01
+      case 2 => FizzBuzzType02
+      case 3 => FizzBuzzType03
+      case _ => throw new RuntimeException("引数は1から3までです")
+    }
+    val fizzbuzz = new FizzBuzz(typeStrategy)
     fizzbuzz.generate(number)
   }
   
   def createList(start: Int, end: Int): Array[String] = {
-    val fizzbuzz = new FizzBuzz(1)
+    val fizzbuzz = new FizzBuzz(FizzBuzzType01)
     fizzbuzz.createList(start, end)
   }
 }
