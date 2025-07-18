130b35eaa33fa1c0855af31f07b963b25614314c
k2works <kakimomokuri@gmail.com>
2025-07-18 00:41:06 +0000

    test: 5を渡したら文字列Buzzを返す, 15を渡したら文字列FizzBuzzを返す

--- app/fizzbuzz-scala/src/main/scala/FizzBuzz.scala
+++ app/fizzbuzz-scala/src/main/scala/FizzBuzz.scala
@@ -1,7 +1,11 @@
 object FizzBuzz {
   def generate(number: Int): String = {
-    if (number % 3 == 0) {
+    if (number % 3 == 0 && number % 5 == 0) {
+      "FizzBuzz"
+    } else if (number % 3 == 0) {
       "Fizz"
+    } else if (number % 5 == 0) {
+      "Buzz"
     } else {
       number.toString
     }
--- app/fizzbuzz-scala/src/test/scala/FizzBuzzTest.scala
+++ app/fizzbuzz-scala/src/test/scala/FizzBuzzTest.scala
@@ -15,4 +15,12 @@ class FizzBuzzTest extends AnyFlatSpec with Matchers {
   it should "3を渡したら文字列Fizzを返す" in {
     fizzbuzz.generate(3) shouldEqual "Fizz"
   }
+
+  it should "5を渡したら文字列Buzzを返す" in {
+    fizzbuzz.generate(5) shouldEqual "Buzz"
+  }
+
+  it should "15を渡したら文字列FizzBuzzを返す" in {
+    fizzbuzz.generate(15) shouldEqual "FizzBuzz"
+  }
 }
