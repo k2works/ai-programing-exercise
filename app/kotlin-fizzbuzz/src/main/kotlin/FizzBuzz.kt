bf8809981d91621b6d2ddd709146d7c9605315fa
k2works <kakimomokuri@gmail.com>
2025-07-18 07:08:55 +0000

    test: 15の倍数の場合にFizzBuzzを返す

--- app/kotlin-fizzbuzz/src/main/kotlin/FizzBuzz.kt
+++ app/kotlin-fizzbuzz/src/main/kotlin/FizzBuzz.kt
@@ -1,7 +1,9 @@
 class FizzBuzz {
     companion object {
         fun generate(number: Int): String {
-            return if (number % 3 == 0) {
+            return if (number % 15 == 0) {
+                "FizzBuzz"
+            } else if (number % 3 == 0) {
                 "Fizz"
             } else if (number % 5 == 0) {
                 "Buzz"
--- app/kotlin-fizzbuzz/src/test/kotlin/FizzBuzzTest.kt
+++ app/kotlin-fizzbuzz/src/test/kotlin/FizzBuzzTest.kt
@@ -29,4 +29,9 @@ class FizzBuzzTest {
     fun `test_5を渡したら文字列Buzzを返す`() {
         assertEquals("Buzz", fizzbuzz.generate(5))
     }
+
+    @Test
+    fun `test_15を渡したら文字列FizzBuzzを返す`() {
+        assertEquals("FizzBuzz", fizzbuzz.generate(15))
+    }
 }
