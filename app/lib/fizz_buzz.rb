4987c2c0962cdc87134b995bec3f62c2b2f567f5
k2works <kakimomokuri@gmail.com>
2025-07-19 00:22:56 +0000

    refactor: setterの削除

--- app/lib/fizz_buzz.rb
+++ app/lib/fizz_buzz.rb
@@ -1,21 +1,34 @@
 class FizzBuzz
   MAX_NUMBER = 100
+  attr_reader :list, :type
 
-  def self.generate(number, type = 1)
-    case type
+  def initialize(type)
+    @type = type
+  end
+
+  def generate(number)
+    is_fizz = number.modulo(3).zero?
+    is_buzz = number.modulo(5).zero?
+
+    case @type
     when 1
-      is_fizz = number.modulo(3).zero?
-      is_buzz = number.modulo(5).zero?
-
       return 'FizzBuzz' if is_fizz && is_buzz
       return 'Fizz' if is_fizz
       return 'Buzz' if is_buzz
 
       number.to_s
+    when 2
+      number.to_s
+    when 3
+      return 'FizzBuzz' if is_fizz && is_buzz
+
+      number.to_s
+    else
+      raise '該当するタイプは存在しません'
     end
   end
 
-  def self.generate_list
-    (1..MAX_NUMBER).map { |n| generate(n) }
+  def generate_list
+    @list = (1..MAX_NUMBER).map { |n| generate(n) }
   end
 end
--- app/test/fizz_buzz_test.rb
+++ app/test/fizz_buzz_test.rb
@@ -11,7 +11,7 @@ class FizzBuzzTest < Minitest::Test
   describe '数を文字列にして返す' do
     describe 'タイプ1の場合' do
       def setup
-        @fizzbuzz = FizzBuzz
+        @fizzbuzz = FizzBuzz.new(1)
       end
 
       describe '三の倍数の場合' do
@@ -40,7 +40,9 @@ class FizzBuzzTest < Minitest::Test
 
       describe '1から100までのFizzBuzzの配列を返す' do
         def setup
-          @result = FizzBuzz.generate_list
+          fizzbuzz = FizzBuzz.new(1)
+          fizzbuzz.generate_list
+          @result = fizzbuzz.list
         end
 
         def test_配列の初めは文字列の1を返す
@@ -64,85 +66,79 @@ class FizzBuzzTest < Minitest::Test
         end
       end
     end
-  end
 
-  describe '配列や繰り返し処理を理解する' do
-    def test_繰り返し処理
-      $stdout = StringIO.new
-      [1, 2, 3].each { |i| p i * i }
-      output = $stdout.string
+    describe 'タイプ2の場合' do
+      def setup
+        @fizzbuzz = FizzBuzz.new(2)
+      end
 
-      assert_equal "1\n4\n9\n", output
+      describe '三の倍数の場合' do
+        def test_3を渡したら文字列3を返す
+          assert_equal '3', @fizzbuzz.generate(3)
+        end
+      end
+
+      describe '五の倍数の場合' do
+        def test_5を渡したら文字列5を返す
+          assert_equal '5', @fizzbuzz.generate(5)
+        end
+      end
+
+      describe '三と五の倍数の場合' do
+        def test_15を渡したら文字列15を返す
+          assert_equal '15', @fizzbuzz.generate(15)
+        end
+      end
+
+      describe 'その他の場合' do
+        def test_1を渡したら文字列1を返す
+          assert_equal '1', @fizzbuzz.generate(1)
+        end
+      end
     end
 
-    def test_selectメソッドで特定の条件を満たす要素だけを配列に入れて返す
-      result = [1.1, 2, 3.3, 4].select(&:integer?)
-      assert_equal [2, 4], result
+    describe 'タイプ3の場合' do
+      def setup
+        @fizzbuzz = FizzBuzz.new(3)
+      end
+
+      describe '三の倍数の場合' do
+        def test_3を渡したら文字列3を返す
+          assert_equal '3', @fizzbuzz.generate(3)
+        end
+      end
+
+      describe '五の倍数の場合' do
+        def test_5を渡したら文字列5を返す
+          assert_equal '5', @fizzbuzz.generate(5)
+        end
+      end
+
+      describe '三と五の倍数の場合' do
+        def test_15を渡したら文字列FizzBuzzを返す
+          assert_equal 'FizzBuzz', @fizzbuzz.generate(15)
+        end
+      end
+
+      describe 'その他の場合' do
+        def test_1を渡したら文字列1を返す
+          assert_equal '1', @fizzbuzz.generate(1)
+        end
+      end
     end
 
-    def test_find_allメソッドで特定の条件を満たす要素だけを配列に入れて返す
-      result = [1.1, 2, 3.3, 4].find_all(&:integer?)
-      assert_equal [2, 4], result
-    end
+    describe 'それ以外のタイプの場合' do
+      def setup
+        @fizzbuzz = FizzBuzz.new(4)
+      end
 
-    def test_特定の条件を満たさない要素だけを配列に入れて返す
-      result = [1.1, 2, 3.3, 4].reject(&:integer?)
-      assert_equal [1.1, 3.3], result
-    end
+      def test_例外を返す
+        e = assert_raises RuntimeError do
+          @fizzbuzz.generate(1)
+        end
 
-    def test_mapメソッドで新しい要素の配列を返す
-      result = %w[apple orange pineapple strawberry].map(&:size)
-      assert_equal [5, 6, 9, 10], result
-    end
-
-    def test_collectメソッドで新しい要素の配列を返す
-      result = %w[apple orange pineapple strawberry].collect(&:size)
-      assert_equal [5, 6, 9, 10], result
-    end
-
-    def test_findメソッドで配列の中から条件に一致する要素を取得する
-      result = %w[apple orange pineapple strawberry].find(&:size)
-      assert_equal 'apple', result
-    end
-
-    def test_detectメソッドで配列の中から条件に一致する要素を取得する
-      result = %w[apple orange pineapple strawberry].detect(&:size)
-      assert_equal 'apple', result
-    end
-
-    def test_指定した評価式で並び変えた配列を返す
-      result1 = %w[2 4 13 3 1 10].sort
-      result2 = %w[2 4 13 3 1 10].sort { |a, b| a.to_i <=> b.to_i }
-      result3 = %w[2 4 13 3 1 10].sort { |b, a| a.to_i <=> b.to_i }
-
-      assert_equal %w[1 10 13 2 3 4], result1
-      assert_equal %w[1 2 3 4 10 13], result2
-      assert_equal %w[13 10 4 3 2 1], result3
-    end
-
-    def test_grepメソッドで配列の中から条件に一致する要素を取得する
-      result = %w[apple orange pineapple strawberry apricot].grep(/^a/)
-      assert_equal %w[apple apricot], result
-    end
-
-    def test_ブロック内の条件式が真である間までの要素を返す
-      result = [1, 2, 3, 4, 5, 6, 7, 8, 9].take_while { |item| item < 6 }
-      assert_equal [1, 2, 3, 4, 5], result
-    end
-
-    def test_ブロック内の条件式が真である以降の要素を返す
-      result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].drop_while { |item| item < 6 }
-      assert_equal [6, 7, 8, 9, 10], result
-    end
-
-    def test_injectメソッドで畳み込み演算を行う
-      result = [1, 2, 3, 4, 5].inject(0) { |total, n| total + n }
-      assert_equal 15, result
-    end
-
-    def test_reduceメソッドで畳み込み演算を行う
-      result = [1, 2, 3, 4, 5].reduce { |total, n| total + n }
-      assert_equal 15, result
+        assert_equal '該当するタイプは存在しません', e.message
+      end
     end
   end
 end
