6a77eae65e2994d416ad81f0871a79dcf5129006
k2works <kakimomokuri@gmail.com>
2025-07-23 09:23:41 +0000

    chore: 静的コード解析ツールCredoの導入とモジュールドキュメント追加

--- app/fizz_buzz/lib/fizz_buzz.ex
+++ app/fizz_buzz/lib/fizz_buzz.ex
@@ -1,4 +1,13 @@
 defmodule FizzBuzz do
+  @moduledoc """
+  FizzBuzz問題を解くためのモジュールです。
+
+  数値を受け取り、以下のルールに従って文字列を返します：
+  - 3で割り切れる場合: "Fizz"
+  - 5で割り切れる場合: "Buzz"  
+  - 3と5の両方で割り切れる場合: "FizzBuzz"
+  - それ以外: 数値の文字列表現
+  """
   def generate(number) when rem(number, 15) == 0, do: "FizzBuzz"
   def generate(number) when rem(number, 3) == 0, do: "Fizz"
   def generate(number) when rem(number, 5) == 0, do: "Buzz"
--- app/fizz_buzz/mix.exs
+++ app/fizz_buzz/mix.exs
@@ -7,7 +7,14 @@ defmodule FizzBuzz.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +28,9 @@ defmodule FizzBuzz.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
