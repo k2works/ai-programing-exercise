fb97b9b0d8013b97161ded5b5d25c2cfc3e29f03
k2works <kakimomokuri@gmail.com>
2025-07-23 05:44:01 +0000

    refactor: メソッドの抽出

--- app/fizz_buzz/test/fizz_buzz_test.exs
+++ app/fizz_buzz/test/fizz_buzz_test.exs
@@ -1,11 +1,13 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
 
-  test "1を渡したら文字列1を返す" do
-    assert FizzBuzz.generate(1) == "1"
-  end
+  describe "FizzBuzz" do
+    test "1を渡したら文字列1を返す" do
+      assert FizzBuzz.generate(1) == "1"
+    end
 
-  test "2を渡したら文字列2を返す" do
-    assert FizzBuzz.generate(2) == "2"
+    test "2を渡したら文字列2を返す" do
+      assert FizzBuzz.generate(2) == "2"
+    end
   end
 end
