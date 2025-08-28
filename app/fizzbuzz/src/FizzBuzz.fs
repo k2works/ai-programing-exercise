a61ee7ba2c693c966952c7a1aad02c148878e6bd
k2works <kakimomokuri@gmail.com>
2025-08-28 09:42:06 +0000

    refactor: メソッド名の変更とメソッドの移動
    - refactor: メソッド名の変更
    - GenerateメソッドをToValueメソッドに変更
    - refactor: メソッドの移動
    - 判定ロジックを各タイプクラスに移動してIsMatchメソッドを実装
    - ファクトリーパターンによる型選択ロジックの改善

--- app/fizzbuzz/src/FizzBuzz.fs
+++ app/fizzbuzz/src/FizzBuzz.fs
@@ -5,47 +5,52 @@ let private isDivisibleBy divisor number = number % divisor = 0
 // スーパークラスの抽出: 基底クラスと各タイプのクラス
 [<AbstractClass>]
 type FizzBuzzType() =
-    abstract member Generate: int -> string
+    abstract member ToValue: int -> string
+    abstract member IsMatch: int -> bool
 
 type FizzBuzzType01() =
     inherit FizzBuzzType()
     static let instance = lazy (FizzBuzzType01())
     static member Instance = instance.Value
-    override this.Generate(number) = string number
+    override this.ToValue(number) = string number
+    override this.IsMatch(number) = not (isDivisibleBy 3 number || isDivisibleBy 5 number)
 
 type FizzBuzzType02() =
     inherit FizzBuzzType()
     static let instance = lazy (FizzBuzzType02())
     static member Instance = instance.Value
-    override this.Generate(number) = "Fizz"
+    override this.ToValue(number) = "Fizz"
+    override this.IsMatch(number) = isDivisibleBy 3 number && not (isDivisibleBy 5 number)
 
 type FizzBuzzType03() =
     inherit FizzBuzzType()
     static let instance = lazy (FizzBuzzType03())
     static member Instance = instance.Value
-    override this.Generate(number) = "Buzz"
+    override this.ToValue(number) = "Buzz"
+    override this.IsMatch(number) = isDivisibleBy 5 number && not (isDivisibleBy 3 number)
 
 type FizzBuzzType04() =
     inherit FizzBuzzType()
     static let instance = lazy (FizzBuzzType04())
     static member Instance = instance.Value
-    override this.Generate(number) = "FizzBuzz"
+    override this.ToValue(number) = "FizzBuzz"
+    override this.IsMatch(number) = isDivisibleBy 3 number && isDivisibleBy 5 number
 
 // オブジェクト指向アプローチ: FizzBuzzクラス
 type FizzBuzz(max: int) =
+    let allTypes = [
+        FizzBuzzType04.Instance :> FizzBuzzType
+        FizzBuzzType02.Instance :> FizzBuzzType
+        FizzBuzzType03.Instance :> FizzBuzzType
+        FizzBuzzType01.Instance :> FizzBuzzType
+    ]
+    
     let getType number =
-        let isDivisibleBy3 = isDivisibleBy 3 number
-        let isDivisibleBy5 = isDivisibleBy 5 number
-        
-        match (isDivisibleBy3, isDivisibleBy5) with
-        | (true, true) -> FizzBuzzType04.Instance :> FizzBuzzType
-        | (true, false) -> FizzBuzzType02.Instance :> FizzBuzzType
-        | (false, true) -> FizzBuzzType03.Instance :> FizzBuzzType
-        | (false, false) -> FizzBuzzType01.Instance :> FizzBuzzType
+        allTypes |> List.find (fun t -> t.IsMatch(number))
     
     let getValue number =
         let typeInstance = getType number
-        typeInstance.Generate(number)
+        typeInstance.ToValue(number)
     
     let _list = [|1..max|] |> Array.map getValue
     
