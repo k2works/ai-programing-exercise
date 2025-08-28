44a7dcf890b08fd84b9c34398b3db840365dd774
k2works <kakimomokuri@gmail.com>
2025-08-28 09:39:49 +0000

    refactor(WIP): ポリモーフィズムによる条件記述の置き換えと関連リファクタリング
    - refactor(WIP): ポリモーフィズムによる条件記述の置き換え
    - インターフェースIFizzBuzzTypeを導入し各タイプクラスで実装
    - refactor: State/Strategyパターンによるタイプコードの置き換え
    - シングルトンパターンでインスタンスを再利用
    - refactor: スーパークラスの抽出
    - 抽象基底クラスFizzBuzzTypeを導入して継承構造を構築

--- app/fizzbuzz/src/FizzBuzz.fs
+++ app/fizzbuzz/src/FizzBuzz.fs
@@ -2,17 +2,50 @@ module FizzBuzz
 
 let private isDivisibleBy divisor number = number % divisor = 0
 
+// スーパークラスの抽出: 基底クラスと各タイプのクラス
+[<AbstractClass>]
+type FizzBuzzType() =
+    abstract member Generate: int -> string
+
+type FizzBuzzType01() =
+    inherit FizzBuzzType()
+    static let instance = lazy (FizzBuzzType01())
+    static member Instance = instance.Value
+    override this.Generate(number) = string number
+
+type FizzBuzzType02() =
+    inherit FizzBuzzType()
+    static let instance = lazy (FizzBuzzType02())
+    static member Instance = instance.Value
+    override this.Generate(number) = "Fizz"
+
+type FizzBuzzType03() =
+    inherit FizzBuzzType()
+    static let instance = lazy (FizzBuzzType03())
+    static member Instance = instance.Value
+    override this.Generate(number) = "Buzz"
+
+type FizzBuzzType04() =
+    inherit FizzBuzzType()
+    static let instance = lazy (FizzBuzzType04())
+    static member Instance = instance.Value
+    override this.Generate(number) = "FizzBuzz"
+
 // オブジェクト指向アプローチ: FizzBuzzクラス
 type FizzBuzz(max: int) =
-    let getValue number =
+    let getType number =
         let isDivisibleBy3 = isDivisibleBy 3 number
         let isDivisibleBy5 = isDivisibleBy 5 number
         
         match (isDivisibleBy3, isDivisibleBy5) with
-        | (true, true) -> "FizzBuzz"
-        | (true, false) -> "Fizz"
-        | (false, true) -> "Buzz"
-        | (false, false) -> string number
+        | (true, true) -> FizzBuzzType04.Instance :> FizzBuzzType
+        | (true, false) -> FizzBuzzType02.Instance :> FizzBuzzType
+        | (false, true) -> FizzBuzzType03.Instance :> FizzBuzzType
+        | (false, false) -> FizzBuzzType01.Instance :> FizzBuzzType
+    
+    let getValue number =
+        let typeInstance = getType number
+        typeInstance.Generate(number)
     
     let _list = [|1..max|] |> Array.map getValue
     
