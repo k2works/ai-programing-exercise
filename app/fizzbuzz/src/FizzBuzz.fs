97c157734a43b41f9a2ee8802bdece1b2b467a71
k2works <kakimomokuri@gmail.com>
2025-08-28 09:43:07 +0000

    refactor: オブジェクトによるプリミティブの置き換え
    - Value Object: FizzBuzzValueクラスを導入
    - 戻り値をプリミティブ文字列からオブジェクトに変更
    - Equals、GetHashCode、ToStringメソッドを実装
    - 後方互換性のため最終的にはValueプロパティで文字列を取得

--- app/fizzbuzz/src/FizzBuzz.fs
+++ app/fizzbuzz/src/FizzBuzz.fs
@@ -2,38 +2,48 @@ module FizzBuzz
 
 let private isDivisibleBy divisor number = number % divisor = 0
 
+// Value Object: FizzBuzzValue
+type FizzBuzzValue(value: string) =
+    member this.Value = value
+    override this.ToString() = value
+    override this.Equals(other) =
+        match other with
+        | :? FizzBuzzValue as other -> this.Value = other.Value
+        | _ -> false
+    override this.GetHashCode() = value.GetHashCode()
+
 // スーパークラスの抽出: 基底クラスと各タイプのクラス
 [<AbstractClass>]
 type FizzBuzzType() =
-    abstract member ToValue: int -> string
+    abstract member ToValue: int -> FizzBuzzValue
     abstract member IsMatch: int -> bool
 
 type FizzBuzzType01() =
     inherit FizzBuzzType()
     static let instance = lazy (FizzBuzzType01())
     static member Instance = instance.Value
-    override this.ToValue(number) = string number
+    override this.ToValue(number) = FizzBuzzValue(string number)
     override this.IsMatch(number) = not (isDivisibleBy 3 number || isDivisibleBy 5 number)
 
 type FizzBuzzType02() =
     inherit FizzBuzzType()
     static let instance = lazy (FizzBuzzType02())
     static member Instance = instance.Value
-    override this.ToValue(number) = "Fizz"
+    override this.ToValue(number) = FizzBuzzValue("Fizz")
     override this.IsMatch(number) = isDivisibleBy 3 number && not (isDivisibleBy 5 number)
 
 type FizzBuzzType03() =
     inherit FizzBuzzType()
     static let instance = lazy (FizzBuzzType03())
     static member Instance = instance.Value
-    override this.ToValue(number) = "Buzz"
+    override this.ToValue(number) = FizzBuzzValue("Buzz")
     override this.IsMatch(number) = isDivisibleBy 5 number && not (isDivisibleBy 3 number)
 
 type FizzBuzzType04() =
     inherit FizzBuzzType()
     static let instance = lazy (FizzBuzzType04())
     static member Instance = instance.Value
-    override this.ToValue(number) = "FizzBuzz"
+    override this.ToValue(number) = FizzBuzzValue("FizzBuzz")
     override this.IsMatch(number) = isDivisibleBy 3 number && isDivisibleBy 5 number
 
 // オブジェクト指向アプローチ: FizzBuzzクラス
@@ -52,20 +62,21 @@ type FizzBuzz(max: int) =
         let typeInstance = getType number
         typeInstance.ToValue(number)
     
-    let _list = [|1..max|] |> Array.map getValue
+    let _list = [|1..max|] |> Array.map (getValue >> (fun v -> v.Value))
     
     member this.List = _list
 
 // 後方互換性のための関数版
 let fizz_buzz number =
-    let isDivisibleBy3 = isDivisibleBy 3 number
-    let isDivisibleBy5 = isDivisibleBy 5 number
-    
-    match (isDivisibleBy3, isDivisibleBy5) with
-    | (true, true) -> "FizzBuzz"
-    | (true, false) -> "Fizz"
-    | (false, true) -> "Buzz"
-    | (false, false) -> string number
+    let allTypes = [
+        FizzBuzzType04.Instance :> FizzBuzzType
+        FizzBuzzType02.Instance :> FizzBuzzType
+        FizzBuzzType03.Instance :> FizzBuzzType
+        FizzBuzzType01.Instance :> FizzBuzzType
+    ]
+    let typeInstance = allTypes |> List.find (fun t -> t.IsMatch(number))
+    let value = typeInstance.ToValue(number)
+    value.Value
 
 let create_numbers () = [| 1..100 |]
 
